44ef0b7ebc8b4bab8c52d10ebf02710400fe79d8
Amirmohammad Tavakkoli <aTavakkoli@spsplan.local>
2018-10-04 21:33:12 +0330

    First commit of SpringDataRestStepByStep

---
+++ src/main/java/ir/brochure/SpringDataRestStepByStep/SpringDataRestStepByStepApplication.java
@@ -0,0 +1,13 @@
+package ir.brochure.SpringDataRestStepByStep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestStepByStepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestStepByStepApplication.class, args);
+	}
+
+}
